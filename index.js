53575f43303a62157bd48ab0a984917d4439011e
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-07-12 17:42:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, token } = req.body;
+  console.log(req.body);
+  try {
+    const payment = await stripe.charges.create({
+      amount,
+      currency: "USD",
+      source: token.id,
+    });
+    console.log("Payment", payment);
+    res.json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
+});
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Sever is listening on port 4000");
+});
